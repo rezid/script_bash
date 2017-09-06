e515f726ce54f4075de7cd386cad637ce77ccd47
Zidane Rezzak <zidane.rezzak@gmail.com>
2017-09-06 08:14:15 -0700

    use read cmd

---
+++ basic/input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ask user for his name
+
+echo Hello, who i am talking to ? 
+
+read varname
+
+echo it\'s nice to meet you $varname
---
+++ basic/input_prompt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ask for username and password
+
+read -p 'Username: ' uservar
+
+read -sp 'Password: ' passvar
+
+echo
+
+echo Thank you $uservar, we have your login details
